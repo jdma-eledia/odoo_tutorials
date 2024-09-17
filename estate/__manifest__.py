e53be19e7e40b1d5f29260efb5a3469c90076927
jdemare <jdemare@eledia.de>
2024-09-17 16:19:13 +0200

    Add security config, views and menus

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,5 +1,10 @@
 {
-    "name": "Estate Module",
+    "name": "Real Estate Module",
     "depends": ["base"],
     "application": True,
+    "data": [
+        "security/ir.model.access.csv",
+        "views/estate_property_views.xml",
+        "views/estate_menus.xml",
+    ],
 }
--- estate/models/property.py
+++ estate/models/property.py
@@ -8,10 +8,12 @@ class TestModel(models.Model):
     name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(
+        copy=False, default=fields.Datetime.add(fields.Datetime.now(), months=3)
+    )
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
@@ -20,4 +22,17 @@ class TestModel(models.Model):
     garden_orientation = fields.Selection(
         [("north", "North"), ("south", "South"), ("east", "East"), ("west", "West")],
         string="Garden Orientation",
+    )
+    active = fields.Boolean(default=True)
+    state = fields.Selection(
+        [
+            ("new", "New"),
+            ("offer received", "Offer Received"),
+            ("offer accepted", "Offer Accepted"),
+            ("sold", "Sold"),
+            ("canceled", "Canceled"),
+        ],
+        required=True,
+        copy=False,
+        default="new",
     )
