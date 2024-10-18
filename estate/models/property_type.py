ff3603f8019d6d570dbc33e5fcb76a9c0bd8677c
jdemare <jdemare@eledia.de>
2024-10-18 10:40:12 +0200

    add sequence order to property.type with handle widget

--- estate/models/property_type.py
+++ estate/models/property_type.py
@@ -4,7 +4,7 @@ from odoo import models, fields
 class PropertyType(models.Model):
     _name = "estate.property.type"
     _description = "Property Type Model"
-    _order = "name"
+    _order = "sequence, name"
 
     name = fields.Char(required=True)
     _sql_constraints = [
@@ -13,3 +13,4 @@ class PropertyType(models.Model):
     property_ids = fields.One2many(
         "estate.property", "property_type_id", string="Properties", copy=False
     )
+    sequence = fields.Integer('sequence', default=1, help="Sequence of the property type")	
